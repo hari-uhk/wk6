f324310518c2262d2c12f0d3e541ea2308d1acf7
hari-uhk <hari.uggini@kellogg.northwestern.edu>
2021-02-18 13:06:10 +0530

    step2

--- movies.js
+++ movies.js
@@ -38,7 +38,15 @@ window.addEventListener('DOMContentLoaded', async function(event) {
   //   <a href="#" class="watched-button block text-center text-white bg-green-500 mt-4 px-4 py-2 rounded">I've watched this!</a>
   // </div>
   // ⬇️ ⬇️ ⬇️
-
+for(i=0; i<movies.length;i++){
+  let movie = movies[i]
+  document.querySelector('.movies').insertAdjacentHTML('beforeend',`
+  <div class="w-1/5 p-4 ${movie.id}">
+    <img src="https://image.tmdb.org/t/p/w500/${movie.poster_path}" class="w-full">
+    <a href="#" class="watched-button block text-center text-white bg-green-500 mt-4 px-4 py-2 rounded">I've watched this!</a>
+  </div>
+`)
+}
   // ⬆️ ⬆️ ⬆️ 
   // End Step 2
 
